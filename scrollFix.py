c4c73e6e94a141fd66572ca8cdea985874361b3b
Julio Cesar <raikyryu1901@gmail.com>
2024-04-03 23:01:20 -0300

    Fixes, new ico

--- scrollFix.py
+++ scrollFix.py
@@ -1,15 +1,17 @@
 import keyboard
 import pyautogui
 
-def checkScrollUp(key):
+
+def check_scroll_up(key):
     if key.name == '[':
         pyautogui.scroll(100)
 
-def checkScrollDown(key):
+
+def check_scroll_down(key):
     if key.name == ']':
         pyautogui.scroll(-100)
 
-keyboard.hook(checkScrollUp)
-keyboard.hook(checkScrollDown)
 
+keyboard.hook(check_scroll_up)
+keyboard.hook(check_scroll_down)
 keyboard.wait()
